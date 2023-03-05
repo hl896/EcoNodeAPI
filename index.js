1fb7db7db77b4ea470cb72093ec59c3f3f7222da
hl896 <hl896@uowmail.edu.au>
2023-03-05 14:13:22 +0000

    all done

--- index.js
+++ index.js
@@ -14,20 +14,19 @@ const cors = require("cors")
 
 dotenv.config();
 
-
+app.use(cors())
 mongoose.connect(process.env.MONGO_URL)
     .then(()=>console.log("DBConnection Successful"))
     .catch((err) => {
         console.log(err);
     });
 
-app.use(cors(
-    {
-        origin:'*',
-        allowedHeaders:'*'
-
-    }
-))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.json());
 app.use("/api/users", userRoute);
