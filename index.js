af29f5356585c0b66f1edfbdfccaf65e67fbe003
hl896 <hl896@uowmail.edu.au>
2023-06-03 12:29:06 +0000

    R.envs

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use("/api/carts", cartRoute);
 app.use("/api/checkout", stripeRoute);
 
 
-app.listen(process.env.PORT||5000, () => {
+app.listen(process.env.PORT||443, () => {
     console.log("backend server is running")
 
 
